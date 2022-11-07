9f3ca0e370f5548bb2596690ad93c266a2fc4368
José <jruedat@unal.edu.co>
2022-11-07 12:34:11 -0500

    Revert "Dynamic connection"
    This reverts commit 1f013430eb05fbc563b9fb9d65033d082d921c7c.

--- Trabajo 3/src/Main.java
+++ Trabajo 3/src/Main.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+
         ArrayList trin = ConsultarDatos.consultarDatos("sucursal");
         System.out.println(trin);
+
     }
 }
